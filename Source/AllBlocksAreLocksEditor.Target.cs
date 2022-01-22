61c9c7025002cb69b1d0f55b2db2c40da4637236
Bash <sebastiankurg@gmail.com>
2022-01-21 20:58:27 -0500

    Another memory allocation increase, this time for the editor.

--- Source/AllBlocksAreLocksEditor.Target.cs
+++ Source/AllBlocksAreLocksEditor.Target.cs
@@ -10,5 +10,9 @@ public class AllBlocksAreLocksEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.Add("AllBlocksAreLocks");
+		
+		// Allocate more memory to prevent build error:
+		// Failed to create virtual memory for PCH
+		WindowsPlatform.PCHMemoryAllocationFactor = 2000;
 	}
 }
