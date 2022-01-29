30c4f226cf3d8802513605d5954a5b6bdf54070b
Jonathan Lawrence <sorosune@gmail.com>
2022-01-28 22:22:47 -0500

    level exit no crash

--- Source/Core/GameplayObjects/LevelExit.cpp
+++ Source/Core/GameplayObjects/LevelExit.cpp
@@ -21,9 +21,8 @@ void ALevelExit::BeginPlay()
 void ALevelExit::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	ABlocksPlayer* Player = Cast<ABlocksPlayer>(OtherActor);
-	if (Player && (Player->bHasKey || !bRequiresKey))
+	if (Player && (Player->bHasKey || !bRequiresKey) && LevelToLoad.Len())
 	{
-		SCREENMSG("Load Next Level");
 		UGameplayStatics::OpenLevel(GetWorld(), FName(*LevelToLoad));
 	}
 }
